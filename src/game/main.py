905c367ddd3d8d6356a32d1a45483d4968ea7cb0
helenaangela <helena.angela.kiisler@ut.ee>
2024-10-30 23:53:41 +0200

    lisasin main.py mängu avamise, mängu akna ja ekraani värskenduse loogika

--- src/game/main.py
+++ src/game/main.py
@@ -13,3 +13,39 @@
 # Lisakommentaar (nt käivitusjuhend)
 #
 ##################################################
+import pygame
+import sys
+
+from pygame.examples.moveit import WIDTH, HEIGHT
+from pygame.examples.sprite_texture import clock, running, event
+
+pygame.init()
+
+WIDTH, HEIGHT = 800, 600
+screen = pygame.display.set_mode(WIDTH, HEIGHT)
+pygame.display.set_caption("BAAR ")
+
+FPS = 60
+clock = pygame.time.Clock()
+
+TUME_PRUUN = (101, 67, 33)
+
+#põhitsükkel
+running = True
+while running:
+    for sündmus in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.quit()
+            sys.exit()
+
+    screen.fill(TUME_PRUUN)
+
+    #Siia saab lisada mängu loogika :)
+
+
+
+
+
+    pygame.display.flip()
+    clock.tick(FPS)
+
