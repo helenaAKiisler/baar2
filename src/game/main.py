2205b845dd28b66b53d8fc3af66f8613f7bc19c4
tonissonlauri <tonissonlauri@gmail.com>
2024-10-31 12:06:22 +0200

    Merge remote-tracking branch 'origin/main'

--- src/game/main.py
+++ src/game/main.py
@@ -16,14 +16,14 @@
 import pygame
 import sys
 
-from pygame.examples.moveit import WIDTH, HEIGHT
-from pygame.examples.sprite_texture import clock, running, event
+# from pygame.examples.moveit import WIDTH, HEIGHT
+# from pygame.examples.sprite_texture import clock, running, event
 
 pygame.init()
 
 WIDTH, HEIGHT = 800, 600
-screen = pygame.display.set_mode(WIDTH, HEIGHT)
-pygame.display.set_caption("BAAR ")
+screen = pygame.display.set_mode((WIDTH, HEIGHT))
+pygame.display.set_caption("BAAR")
 
 FPS = 60
 clock = pygame.time.Clock()
@@ -31,21 +31,33 @@ clock = pygame.time.Clock()
 TUME_PRUUN = (101, 67, 33)
 
 #põhitsükkel
-running = True
-while running:
-    for sündmus in pygame.event.get():
+running = False
+paused = False
+while True:
+    for event in pygame.event.get():
         if event.type == pygame.QUIT:
             pygame.quit()
             sys.exit()
-
-    screen.fill(TUME_PRUUN)
+        elif event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_SPACE:
+                if not running: #Alusta mängu
+                    running = True
+                    paused = False
+            elif event.key == pygame.K_p:
+                if running:
+                    paused = not paused  # Pausile või pausilt tagasi
+            elif event.key == pygame.K_ESCAPE:  # ESC klahviga sulgemine
+                pygame.quit()
+                sys.exit()
+    #kui mäng ei ole käimas
+    if not running:
+        continue
+    # kui mäng on pausil, ei uuenda ekraani
+    if not paused:
+        screen.fill(TUME_PRUUN)
 
     #Siia saab lisada mängu loogika :)
 
-
-
-
-
-    pygame.display.flip()
+        pygame.display.flip()
     clock.tick(FPS)
 
