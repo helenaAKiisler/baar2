feb14b04626892247c593c7abb92b1213921ba75
tonissonlauri <tonissonlauri@gmail.com>
2024-11-21 14:47:53 +0200

    Update main.py

--- src/game/main.py
+++ src/game/main.py
@@ -3,48 +3,41 @@
 # 2024/2025 sügissemester
 #
 # Projekt
-# Teema: Baar2
+# Teema: Baar2 mäng
 #
 #
 # Autorid: Helena Angela Kiisler, Lauri Tõnisson
 #
-# mõningane eeskuju:
+# mõningane eeskuju: Internetist leitavad juba koostatud pygame mängud.
 #
 # Lisakommentaar (nt käivitusjuhend)
-#
+# Mängu käivitamiseks tuleb käivitada main.py. Mängija liikumiseks kasuta klaviatuuril nooleklahve või WASD klahvikombinatsioone.
+# Nuppudele vajutamine toimub vasaku hiireklahviga. Vajutades klahvi P läheb mäng pausile.
 ##################################################
 import pygame
 import sys
 import os.path
-import random
-from settings import WIDTH, HEIGHT, FPS, DARK_BROWN, GRAY, WHITE,  PRESET_TABLE_POSITIONS
+from settings import WIDTH, HEIGHT
 from player import Player
 from progress_bar import GameTimer
-from object import Glass, Table, Enemy
-from ui import initialize_font, TEXT_COLOR  # Importime initialize_font ja TEXT_COLOR
+from object import Glass, Table, Enemy, Bar
+from ui import initialize_font # Importime initialize_font
 from pohiloogika import Game
 from scene import Scene
-from bar import Bar
-
-
 
 # Algseaded
 pygame.init()
 pygame.display.set_caption("Baar2")
 clock = pygame.time.Clock()
 GAME_TITLE = "Baar2"
-
 font = pygame.font.SysFont("Arial", 30)
 score = 0
-
 game = Game()
 game.start_game()
-
 current_scene: Scene
-
 bar = Bar
 
-# Mängija pildi tee ja pildi laadimine
+# Mängija ja vastase failitee ja pildi laadimine
 base_path = os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 character_image_path = os.path.join(base_path, "assets", "designs", "character", "mees", "teenindus.mees2.png")
 player_image = pygame.image.load(character_image_path)
@@ -54,6 +47,7 @@ enemy_image = pygame.image.load(enemy_image_path)
 
 # Objektide loomine
 player = Player(WIDTH // 2, HEIGHT - 80, player_image, bar)
+
 # Kood, mis loob lauaobjektid ja edastab need vaenlasele
 tables = pygame.sprite.Group()
 table1 = Table(100, 100)
@@ -64,6 +58,7 @@ enemy = Enemy(200, 80, enemy_image, tables)
 
 game_timer = GameTimer()
 
+# Ekraani stseenivahetus funktsioon
 def scene_switcher(new_scene_name, screen=None):
     from game_level import GameLevel
     global current_scene
@@ -73,16 +68,17 @@ def scene_switcher(new_scene_name, screen=None):
     elif new_scene_name == "GameLevel":
         current_scene = GameLevel(scene_switcher, screen=screen)  # Edastame screen objekti
 
+# Põhifunktsioon
 def main():
     from main_menu import MainMenu
     global current_scene, score, game_timer
-    screen = pygame.display.set_mode((800, 600))  # Loome screen objekti siin
+    screen = pygame.display.set_mode((800, 600))  # Loome screen objekti
 
     # Initsialiseerime FONT enne MainMenu loomist
     initialize_font()
 
     # Algne stseen
-    current_scene = MainMenu(scene_switcher, game_title="Baar 2", screen=screen)  # Edastame game_title ja screen
+    current_scene = MainMenu(scene_switcher, game_title="Baar 2", screen=screen)
 
     # Mängutsükkel
     while current_scene.is_running:
@@ -94,10 +90,10 @@ def main():
                 if event.key == pygame.K_p:  # Pausile minek või pausilt naasmine
                     game.toggle_pause()
                     game_timer.toggle_pause()
-                elif event.key == pygame.K_q:
+                elif event.key == pygame.K_q: #Mängu sulgemine
                     pygame.quit()
                     sys.exit()
-                elif event.key == pygame.K_c and game.is_paused:
+                elif event.key == pygame.K_c and game.is_paused: #Mängu pausile panek
                     game.toggle_pause()
                     game_timer.toggle_pause()
 
