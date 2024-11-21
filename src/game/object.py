b954283ae5d1605890e642b5623c90db85e8a614
tonissonlauri <tonissonlauri@gmail.com>
2024-11-21 14:48:36 +0200

    Update object.py

--- src/game/object.py
+++ src/game/object.py
@@ -1,6 +1,18 @@
+# Mängus olevate objektide klassid mis määravad ära lauad, baari, klaasid ja vastase.
 import pygame
-from settings import GLASS_SIZE, TABLE_SIZE, GRAY, RED
-import random
+from settings import GRAY,WIDTH
+
+
+class Bar(pygame.sprite.Sprite):
+    def __init__(self, width):
+        super().__init__()
+        self.image = pygame.Surface((width, 50))  # Baar on laiem, kõrgus 50px
+        self.image.fill((99, 102, 106))  # Baaril pruun värv
+        self.rect = self.image.get_rect()
+
+        # Paigutame baari ekraani ülaosas keskele
+        self.rect.x = (WIDTH - width) // 2  # Baar on keskendatud horisontaalselt
+        self.rect.y = 50  # Baar asub ekraani ülaservas, kuid natuke allpool
 
 class Table(pygame.sprite.Sprite):
     def __init__(self, x, y):
