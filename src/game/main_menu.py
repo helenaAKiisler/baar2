e225b6131ade2e267141725af507a83caec3aaac
tonissonlauri <tonissonlauri@gmail.com>
2024-12-16 11:20:04 +0200

    start ekraanile mängija ja vaenlase pildi lisamine

--- src/game/main_menu.py
+++ src/game/main_menu.py
@@ -23,16 +23,32 @@ class MainMenu(Scene):
         # Kontrollige nuppe
         self.start_button.handle_events(event)
         self.quit_button.handle_events(event)
+
     # Nuppude paigutamine ekraanile
     def render(self, screen):
+        from main import player_image, enemy_image
         screen.fill(self.BACKGROUND_COLOR)
+
+        # Mängija pildi kuvamine Start nupu juures vasakul
+        player_scaled = pygame.transform.scale(player_image, (120, 120))
+        screen.blit(player_scaled, (WIDTH // 2 - 200, HEIGHT // 2 - 50))
+
+        # Vaenlase pildi kuvamine Quit nupu juures paremal
+        enemy_scaled = pygame.transform.scale(enemy_image, (120, 120))
+        screen.blit(enemy_scaled, (WIDTH // 2 + 110, HEIGHT // 2 - 50))
+
+        # Nuppude kuvamine
         self.start_button.render(screen, (
             (screen.get_width() - self.start_button.get_width()) / 2,
-            screen.get_height() - self.quit_button.get_height() - self.start_button.get_height() - 120))  # Paigutame Start nupu
+            screen.get_height() - self.quit_button.get_height() - self.start_button.get_height() - 120))
+
         self.quit_button.render(screen, (
             (screen.get_width() - self.quit_button.get_width()) / 2,
-            screen.get_height() - self.quit_button.get_height() - 110)) # Paigutame Quit nupu
-        title_text = pygame.font.Font("../../assets/font/InknutAntiqua-Regular.ttf", 48).render(self.game_title, True, (152, 191, 161))
+            screen.get_height() - self.quit_button.get_height() - 110))
+
+        # Tiitel
+        title_text = pygame.font.Font("../../assets/font/InknutAntiqua-Regular.ttf", 48).render(self.game_title, True,
+                                                                                                (152, 191, 161))
         screen.blit(title_text, (WIDTH // 2 - title_text.get_width() // 2, HEIGHT // 4))
 
     def quit_game(self):
@@ -58,6 +74,7 @@ class WinMenu(Scene):
         # Kontrollige nuppe
         self.restart_button.handle_events(event)
         self.quit_button.handle_events(event)
+
     # Nuppude paigutamine ekraanile
     def render(self, screen):
         screen.fill(self.BACKGROUND_COLOR)
